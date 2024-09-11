2a032d14ac4b638a01ef99e600d17ce384f9dbdd
Kim Seonghyeon <kim.seonghyeon@outlook.com>
2024-09-11 13:53:22 +0900

    Initial implementation

---
+++ go.mod
@@ -0,0 +1,17 @@
+module pygoreadability
+
+go 1.22.1
+
+require (
+	github.com/go-python/gopy v0.4.10
+	github.com/go-shiori/dom v0.0.0-20210627111528-4e4722cd0d65
+	github.com/go-shiori/go-readability v0.0.0-20240530203707-15a31cd77abf
+	golang.org/x/net v0.9.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
+	github.com/gogs/chardet v0.0.0-20211120154057-b7413eaefb8f // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
---
+++ pygoreadability.go
@@ -0,0 +1,35 @@
+// pygoreadability.go
+package pygoreadability
+
+import (
+    "fmt"
+	"strings"
+    nurl "net/url"
+
+    readability "github.com/go-shiori/go-readability"
+	"github.com/go-shiori/dom"
+)
+
+func Parse(input string) (readability.Article, error) {
+	parser := readability.NewParser()
+	reader := strings.NewReader(input)
+	doc, err := dom.FastParse(reader)
+	if err != nil {
+		return readability.Article{}, fmt.Errorf("failed to parse input: %v", err)
+	}
+	pageURL, _ := nurl.ParseRequestURI("http://fakehost.com")
+
+	return parser.ParseDocument(doc, pageURL)
+}
+
+func GetTitle(article readability.Article) string {
+	return article.Title
+}
+
+func GetContent(article readability.Article) string {
+	return article.Content
+}
+
+func GetTextContent(article readability.Article) string {
+	return article.TextContent
+}
